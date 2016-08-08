670b934948c69b7701b8cabbe9a5573fa84a4c11
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2016-08-07 21:00:43 -0300

    Basic Functions

---
+++ index.js
@@ -0,0 +1,40 @@
+var math = Math;
+// Basic Functions
+exports.rand = math.random();
+
+exports.min = math.min();
+
+exports.max = math.max();
+
+exports.round = math.round();
+
+exports.roundUp = math.ceil();
+
+exports.roundDown = math.floor();
+
+exports.abs = math.abs();
+
+exports.acos = math.acos();
+
+exports.asin = math.asin();
+
+exports.atan = math.atan();
+
+exports.cos = math.cos();
+
+exports.sin = math.sin();
+
+exports.exp = math.exp();
+
+exports.log = math.log();
+
+exports.pot = math.pow();
+
+exports.sqrt = math.sqrt;
+
+// Constants
+exports.E = math.E;
+
+exports.PI = math.PI;
+
+exports.LN = math.LN2;
